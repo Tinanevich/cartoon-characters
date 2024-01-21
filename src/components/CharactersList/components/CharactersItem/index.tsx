4ab4a5bddde2da17dfe6aba2d35644398dc54c9a
Kristina <Kristina@gmail.com>
2024-01-21 16:13:42 +0300

    fix: modal window

--- src/components/CharactersList/components/CharactersItem/index.tsx
+++ src/components/CharactersList/components/CharactersItem/index.tsx
@@ -12,28 +12,28 @@ interface ICharacterItemProps {
 const CharactersItem: FC<ICharacterItemProps> = ({item})=> {
    const { active, toggle } = useModal();
    return (
-      <div onClick={toggle} className="charactersItem">
+      <main onClick={toggle} className="charactersItem">
          <img className="img" src={item.image} alt={item.image} />
          <p className="name">{item.name}</p>
             <Modal active={active} toggle={toggle}>
-               <span className='modal'>
-                  <img className="img_modal" src={item.image} alt={item.image} />
-                  <p className='gradient_modal'></p>
-                  <p className="info_modal">
-                     <ul className='info-left'>
-                        <li className="name_modal">Name: <p className="content">{item.name}</p></li>
-                        <li className="name_modal">Status: <p className="content">{item.status}</p></li>
-                        <li className="name_modal">Species: <p className="content">{item.species}</p></li>
-                     </ul>
-                     <ul className='info-right'>
-                        <li className="name_modal">Origin: <p className="content">{item.origin.name}</p></li>
-                        <li className="name_modal">Location: <p className="content">{item.location.name}</p></li>
-                        <li className="name_modal">Gender: <p className="content">{item.gender}</p></li>
-                     </ul>
-                  </p>
-               </span>
+               <div className='modal'>
+                  <div className='gradient_modal'></div>
+                     <img className="img_modal" src={item.image} alt={item.image} />
+                     <div className="info_modal">
+                        <ul className='info-left'>
+                           <li className="name_modal">Name: <span className="content">{item.name}</span></li>
+                           <li className="name_modal">Status: <span className="content">{item.status}</span></li>
+                           <li className="name_modal">Species: <span className="content">{item.species}</span></li>
+                        </ul>
+                        <ul className='info-right'>
+                           <li className="name_modal">Origin: <span className="content">{item.origin.name}</span></li>
+                           <li className="name_modal">Location: <span className="content">{item.location.name}</span></li>
+                           <li className="name_modal">Gender: <span className="content">{item.gender}</span></li>
+                        </ul>
+                     </div>
+               </div>
             </Modal>
-      </div>
+      </main>
    )
 }
 
